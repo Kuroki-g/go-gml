74593783aa12ad33ee50b765dcf614210c6111d0
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2026-03-30 07:26:48 +0900

    fix: decodeGeometryProp に StartElement を渡し xlink:href をサイレント欠落させない
    プロパティ要素 (bldg:lod1Solid 等) の xlink:href 属性を検出できるよう
    decodeGeometryProp のシグネチャに se xml.StartElement を追加。
    by-reference ジオメトリを検出した場合は nil を返さずエラーを返す。
    nsXLink 定数と xlinkHref() ヘルパーを追加。

--- citygml2_0/decode_building.go
+++ citygml2_0/decode_building.go
@@ -2,19 +2,31 @@ package citygml2_0
 
 import (
 	"encoding/xml"
+	"fmt"
+	"strings"
 
 	intl "github.com/Kuroki-g/go-gml/citygml2_0/internal"
 	"github.com/Kuroki-g/go-gml/core"
 )
 
 const (
-	nsBldg = "http://www.opengis.net/citygml/building/2.0"
-	nsGML  = "http://www.opengis.net/gml"
+	nsBldg  = "http://www.opengis.net/citygml/building/2.0"
+	nsGML   = "http://www.opengis.net/gml"
+	nsXLink = "http://www.w3.org/1999/xlink"
 )
 
 func gmlID(se xml.StartElement) string {
 	for _, a := range se.Attr {
 		if a.Name.Local == "id" && a.Name.Space == nsGML {
+			return a.Value
+		}
+	}
+	return ""
+}
+
+func xlinkHref(se xml.StartElement) string {
+	for _, a := range se.Attr {
+		if a.Name.Local == "href" && a.Name.Space == nsXLink {
 			return a.Value
 		}
 	}
@@ -40,55 +52,55 @@ func decodeBuilding(dec *xml.Decoder, se xml.StartElement, gmlDec core.Decoder) 
 			}
 			switch t.Name.Local {
 			case "lod0FootPrint":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod0FootPrint = g
 			case "lod0RoofEdge":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod0RoofEdge = g
 			case "lod1Solid":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod1Solid = g
 			case "lod1MultiSurface":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod1MultiSurface = g
 			case "lod1TerrainIntersection":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod1TerrainIntersection = g
 			case "lod2Solid":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod2Solid = g
 			case "lod2MultiSurface":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod2MultiSurface = g
 			case "lod2MultiCurve":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
 				b.Lod2MultiCurve = g
 			case "lod2TerrainIntersection":
-				g, err := decodeGeometryProp(dec, gmlDec)
+				g, err := decodeGeometryProp(dec, t, gmlDec)
 				if err != nil {
 					return nil, err
 				}
@@ -103,8 +115,11 @@ func decodeBuilding(dec *xml.Decoder, se xml.StartElement, gmlDec core.Decoder) 
 }
 
 // decodeGeometryProp decodes the first child GML element of a property wrapper
-// and consumes through the property's EndElement.
-func decodeGeometryProp(dec *xml.Decoder, gmlDec core.Decoder) (*core.Geometry, error) {
+// (se is the property wrapper's StartElement, e.g. <bldg:lod1Solid>).
+// If xlink:href is set on the property element, an error is returned — by-reference
+// geometry is not yet supported.
+func decodeGeometryProp(dec *xml.Decoder, se xml.StartElement, gmlDec core.Decoder) (*core.Geometry, error) {
+	href := xlinkHref(se)
 	var result *core.Geometry
 	for {
 		tok, err := dec.Token()
@@ -113,7 +128,7 @@ func decodeGeometryProp(dec *xml.Decoder, gmlDec core.Decoder) (*core.Geometry, 
 		}
 		switch t := tok.(type) {
 		case xml.StartElement:
-			if result == nil {
+			if result == nil && href == "" {
 				sub, err := intl.ExtractSubtree(dec, t)
 				if err != nil {
 					return nil, err
@@ -129,6 +144,10 @@ func decodeGeometryProp(dec *xml.Decoder, gmlDec core.Decoder) (*core.Geometry, 
 				}
 			}
 		case xml.EndElement:
+			if href != "" {
+				id := strings.TrimPrefix(href, "#")
+				return nil, fmt.Errorf("citygml: %s: xlink:href=%q (id=%q): by-reference geometry not supported", se.Name.Local, href, id)
+			}
 			return result, nil
 		}
 	}
